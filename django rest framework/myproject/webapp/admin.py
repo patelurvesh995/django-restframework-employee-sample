b635ade37554ced9819ee02c3996cf18c5a08449
patelurvesh995 <patelurvesh995@gmail.com>
2020-06-11 13:50:47 +0200

    Djangosalesforceemployeesampleapplication

---
+++ django rest framework/myproject/webapp/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from .models import employees
+#from .models import Contact
+
+admin.site.register(employees)
+#admin.site.register(Contact)
+# Register your models here.
---
+++ django rest framework/myproject/webapp/constants.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+TITLE_CHOICES=(
+    ('MR','MR')
+    ('MRS','MRS')
+    ('MS','MS')
+)
